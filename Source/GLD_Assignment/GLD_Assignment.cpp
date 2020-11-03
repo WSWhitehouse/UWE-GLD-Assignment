d7ef3ce2bb016c3c436325756661296f470d0960
WSWhitehouse <williamswhitehouse@gmail.com>
2020-11-03 18:19:30 +0000

    Imported Empty UE4 Project

---
+++ Source/GLD_Assignment/GLD_Assignment.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GLD_Assignment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GLD_Assignment, "GLD_Assignment" );
